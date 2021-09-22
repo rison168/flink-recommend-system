32cc01c0f0b17a3957e0477040336ac13d1d7dca
Rison <2462301753@qq.com>
2021-09-22 23:00:23 +0800

    [feat] 添加LogMapFunction,完成日志从kafka写入到Hbase

--- flink-2-hbase/src/main/scala/com/rison/flink/client/HbaseClient.scala
+++ flink-2-hbase/src/main/scala/com/rison/flink/client/HbaseClient.scala
@@ -1,15 +1,55 @@
 package com.rison.flink.client
 
-import org.apache.hadoop.hbase.client.{Admin, Connection}
+import java.io.IOException
+
+import com.rison.flink.common.Logger
+import com.rison.flink.util.Property
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
+import org.apache.hadoop.hbase.client.{Admin, Connection, ConnectionFactory, Put, Table}
 
 /**
  * @author : Rison 2021/9/22 下午10:31
  *         Hbase 客户端
  */
-object HbaseClient {
+object HbaseClient extends Logger {
+
   var admin: Admin = _
   var conn: Connection = _
 
+  /**
+   * 初始化
+   */
+  def init(): Unit = {
+    val configuration: Configuration = HBaseConfiguration.create()
+    configuration.set("hbase.rootdir", Property.getStrValue("hbase.rootdir"))
+    configuration.set("hbase.zookeeper.quorum", Property.getStrValue("hbase.zookeeper.quorum"))
+    configuration.set("hbase.client.scanner.timeout.period", Property.getStrValue("hbase.client.scanner.timeout.period"))
+    configuration.set("hbase.rpc.timeout", Property.getStrValue("hbase.rpc.timeout"))
+    try {
+      conn = ConnectionFactory.createConnection(configuration)
+      admin = conn.getAdmin
+    } catch {
+      case e: IOException => logger.warn("Hbase 初始化异常!错误信息为：{}", e.getMessage)
+    }
 
+  }
+
+  /**
+   * 写入数据
+   *
+   * @param tablename  表名
+   * @param rowkey     行号
+   * @param familyname 列簇名
+   * @param column     字段
+   * @param data       数据
+   */
+  def putData(tablename: String, rowkey: String, familyname: String, column: String, data: String) = {
+    init()
+    val table: Table = conn.getTable(TableName.valueOf(tablename))
+    val put = new Put(rowkey.getBytes())
+    put.addColumn(familyname.getBytes(), column.getBytes(), data.getBytes())
+    table.put(put)
+  }
 
 }
--- flink-2-hbase/src/main/scala/com/rison/flink/map/LogMapFunction.scala
+++ flink-2-hbase/src/main/scala/com/rison/flink/map/LogMapFunction.scala
@@ -1,5 +1,6 @@
 package com.rison.flink.map
 
+import com.rison.flink.client.HbaseClient
 import com.rison.flink.common.{LogToEntity, Logger}
 import com.rison.flink.domain.LogEntity
 import org.apache.flink.api.common.functions.MapFunction
@@ -8,15 +9,19 @@ import org.apache.flink.api.common.functions.MapFunction
  * @author : Rison 2021/9/22 下午10:09
  *
  */
-case class LogMapFunction() extends MapFunction[String, LogEntity] with Logger{
-  override def map(logStr: String): LogMapFunction = {
+case class LogMapFunction() extends MapFunction[String, LogEntity] with Logger {
+  override def map(logStr: String): LogEntity = {
     logger.info("日志信息：{}", logStr)
     val logEntity: LogEntity = LogToEntity.getLogEntity(logStr)
     //TODO: 写入到Hbase里面去
-    if (logEntity != null){
-
+    if (logEntity != null) {
+      val rowKey: String = logEntity.userId + "_" + logEntity.productId + "_" + logEntity.time
+      HbaseClient.putData(tablename = "con", rowkey = rowKey, familyname = "log", column = "userid", logEntity.userId.toString)
+      HbaseClient.putData(tablename = "con", rowkey = rowKey, familyname = "log", column = "productid", logEntity.productId.toString)
+      HbaseClient.putData(tablename = "con", rowkey = rowKey, familyname = "log", column = "time", logEntity.time.toString)
+      HbaseClient.putData(tablename = "con", rowkey = rowKey, familyname = "log", column = "action", logEntity.action.toString)
     }
-
+    logEntity
 
   }
 }
